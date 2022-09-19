5c5808a72088ca2e0ce26fb055c19e0fc0458d0c
theocita <theocitaallen@gmail.com>
2022-09-19 18:02:38 -0400

    Complete Assignemnt

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+  return `This Saturday, I want to ${activity}!`;
+};
+
+const mondayWork = function(activity = "go to the office"){
+  return `This Monday, I will ${activity}.`
+};
+
+function wrapAdjective(part1) {
+  return function (part2) {
+    return `You are ${part1}${part2}${part1}!`;
+  };
+};
